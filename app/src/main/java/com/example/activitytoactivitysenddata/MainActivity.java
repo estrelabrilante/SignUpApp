ecd67621b0146c37e08f61710efbb052eef8c20a
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-07-07 15:59:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/activitytoactivitysenddata/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.activitytoactivitysenddata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name,email,phone;
+    Button signup;
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.editTextName);
+        email = findViewById(R.id.editTextEmail);
+        phone = findViewById(R.id.editTextPhone);
+        signup = findViewById(R.id.buttonSignUp);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick( View view ) {
+                String userName = name.getText().toString();
+                String userEmail = email.getText().toString();
+               // int phoneNumber = Integer.valueOf(phone.getText().toString());
+                String phoneNumber = phone.getText().toString();
+                Intent i;
+                i = new Intent( MainActivity.this,SecondActivity.class);
+                i.putExtra("name", userName);
+                i.putExtra("email",userEmail);
+                i.putExtra("phone",phoneNumber);
+
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/activitytoactivitysenddata/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.example.activitytoactivitysenddata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView name,email,phone;
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+         name = findViewById(R.id.textViewName);
+      email = findViewById(R.id.textViewEmail);
+     phone = findViewById(R.id.textViewPhone);
+
+        Intent i = getIntent();
+         String userName = i.getStringExtra("name");
+         String userEmail = i.getStringExtra("email");
+        //int userPhone = i.getIntExtra("phone", 0);
+        int userPhone = i.getIntExtra("phone", 0);
+        name.setText("Hello " + userName);
+        email.setText("Your email address is " + " " + userEmail);
+        // phone.setText("Phone Number " +  "" + String.valueOf(userPhone));
+        phone.setText("Phone Number " + "  " + String.valueOf(userPhone));
+        Toast.makeText(getApplicationContext(), userName,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), userEmail,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), String.valueOf(userPhone),Toast.LENGTH_LONG).show();
+
+
+    }
+}
